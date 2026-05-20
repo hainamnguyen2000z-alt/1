6f3a84988a6b1eb9399936a20c0e269af7b6ff31
hainamnguyen2000z-alt <hainamnguyen2000z@gmail.com>
2026-05-21 06:02:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
+
+# =====================================================
+# Redirect domain DuckDNS → Render HTTPS
+# =====================================================
+@app.before_request
+def redirect_duckdns():
+    host = request.host.lower()
+
+    if "namhai99.duckdns.org" in host:
+        return redirect("https://one-k7o5.onrender.com", code=301)
+
 
 # =====================================================
 # GET /api/hash.php
 # =====================================================
 @app.route("/api/hash.php", methods=["GET"])
 def hash_php():
-
     return "ca07f0971f5dd187fdb952ec09ac8d85eea631f92b0f232d730bc9de219231c6"
 
 
@@ -16,7 +26,6 @@ def hash_php():
 # =====================================================
 @app.route("/api/auth", methods=["GET"])
 def auth():
-
     return "nk0wr0oxw1d5a8n2j49fc26gyinwe6khqf1n1zhwk2ayy3tvv0igf0z435mu"
 
 
@@ -25,7 +34,6 @@ def auth():
 # =====================================================
 @app.route("/api/active_soft_final_20240605", methods=["POST", "GET"])
 def active():
-
     return """MZFjT0A5bkJlYzQjNRVDRM4W8Ro7tJRzYupgHVBJ2tTwkvkJumaNVVkyVnRlVWwzZWtwS1lXUnJTbFZhWWt0UU5URnJWRFpaYzBkRVZGaFhNRVp3WjNwSlVYVlhWbTlIZDNCdkwyRlJRMUkxVEZCRVQyMXpaVzg1ZURaRE16TXdTMVZOTUU0Mk5TOU9NREZRUVZKUVQzWmhRVXBMUkZCeFUzZEtSWGQzUVRWNllURjBNbE5TUW0xd1ZURktaRFZpVlhJelFtcDNWVEJtWW14d1dFcEpWV3BEVDNaV1RqTmplWFYwVG1wUVNsVnBUV3BXU1VoeFlrVlRXRTB2YjNjMWFHWXlRMGxzVUZrd1ZGSnVUMHBRZEhkVFoyWnliVk13TUd4VGFpOTZRMEp3VFZSYU0xaDZRVFE0ZW1OMFdYVTBOWFJ0VTIwNWVXWjBVR3BHVEdsaFFsZElZemxWZFhwQlZFSkJRV3BxVDFacGMxVlNSSGRYYlVwcmRFaHplRzVFUTIwcmRucFNja2RSTW5JMVNVMDJWa3B1U2l0MFlXSmFVRFkzY1hScFF6Rk5RbWRsWkhaT1ZHMVNlVk0yU0UxWVdEWmtXazk2UkROc1VHMXhlRXN5VWpWSFVsSTJkVGhITTFwNWNHcG9VMXA2VkhaVWR6WjBURWhwTlhaeU1YaEtLMGczU0c1U0swWlVRWGNyU0ZwbGNHNWpZbkZGU2xCQ04xTjFMM0ZNZERkMlMwOUlZamMxUmtGUWNtMWpTME4ySzNsSmJHcDRX0hrLXyC3Rkh9tiwBgn8mCha6evOeFffTsZlVlSEZPUVhkSVEbrFG|cbb0c5291e7f628452b9ce7f5e9bda658200df442e9aab3ebf48023887e7097d"""
 
 
